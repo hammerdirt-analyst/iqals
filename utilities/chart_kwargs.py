5e341397826561835d3b03efbd71646bf809bf30
Roger Erismann <roger@hammerdirt.ch>
2021-05-16 22:12:05 +0200

    added population chart

--- utilities/chart_kwargs.py
+++ utilities/chart_kwargs.py
@@ -8,3 +8,5 @@ xlab_k14 = {'labelpad':14, 'fontsize':14}
 xlabels_top = dict(labeltop=True, labelbottom=False, pad=12)
 no_xticks = dict(axis='x', which='both', width=0, length=0 )
 label_r14 = dict(labelpad=14, loc='right', fontsize=14)
+
+rotate_45 = dict(axis='x', which='both', labelrotation=45)
